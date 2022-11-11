9c439552b46d3a6b6fe235c831eb27464749a9e2
hossainshadat <hossainshadat501@gmail.com>
2022-11-11 14:33:49 +0600

    vercel config

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Health coatch server is running");
+});
 
 const uri = process.env.DB_URL;
 
@@ -222,10 +228,8 @@ app.patch("/review/:id", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Health coatch server is running");
-});
-
 app.listen(port, () => {
   console.log(`Healthcoach app listening on port: ${port}`);
 });
+
+module.exports = app;
